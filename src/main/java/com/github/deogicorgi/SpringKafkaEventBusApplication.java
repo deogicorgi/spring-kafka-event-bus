c87a5b63c7cf2c5d7c90934fbf6bd8d81bcb976c
Deogicorgi <deogicorgi@gmail.com>
2023-10-11 21:26:10 +0900

    project init commit

---
+++ src/main/java/com/github/deogicorgi/SpringKafkaEventBusApplication.java
@@ -0,0 +1,13 @@
+package com.github.deogicorgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaEventBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaEventBusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/deogicorgi/SpringKafkaEventBusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deogicorgi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaEventBusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
