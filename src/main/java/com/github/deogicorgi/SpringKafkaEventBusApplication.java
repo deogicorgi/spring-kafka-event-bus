ace99df6a73ee6d1d89849f3d9436c0bb5b641ba
Deogicorgi <deogicorgi@gmail.com>
2023-10-11 21:59:45 +0900

    docker-compose.yml 생성
    - zookeeper, kafka 설정

--- src/main/java/com/github/deogicorgi/SpringKafkaEventBusApplication.java
+++ src/main/java/com/github/deogicorgi/SpringKafkaEventBusApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringKafkaEventBusApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringKafkaEventBusApplication.class, args);
     }
